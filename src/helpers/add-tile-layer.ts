719457e29f26be898b519a73dbcdb878d71ce62d
alena_gluschenko <qlushenko36@gmail.com>
2024-09-27 12:30:11 +0300

    change map style

--- src/helpers/add-tile-layer.ts
+++ src/helpers/add-tile-layer.ts
@@ -1,7 +1,7 @@
 import L from "leaflet";
 
 export const addTileLayer = (map) => {
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: 'Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https://github.com/glush-chenko">Alena Glushchenko</a>.'
     }).addTo(map);
